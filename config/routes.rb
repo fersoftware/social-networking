3fc672c436fcedab44b99457486598a49930bcdc
fersoftware <fersoftware@gmail.com>
2021-06-11 19:24:31 -0300

    crud admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # devise_for :users
   devise_for :admins, skip: %i[:registrations], controllers: {sessions: 'admins/sessions'}
   
   namespace :admin do
     root to: "home#index"
+    resources :admins
   end
 
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
+
   it "admin is valid" do
-    admin = Admin.new(email: "admin@admin.com", password: '123456')
+    admin = Admin.new(name: "Admin First", email: "admin@admin.com", password: '123456')
+    
     expect(admin.email).to eql "admin@admin.com"
     expect(admin).to be_valid
   end
