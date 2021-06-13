f88200e5348f79e18fb19052445a22cec93e08ba
fersoftware <fersoftware@gmail.com>
2021-06-13 14:44:15 -0300

    followers and following part 2

--- app/controllers/user/profile_controller.rb
+++ app/controllers/user/profile_controller.rb
@@ -2,4 +2,16 @@ class User::ProfileController < UserController
     def show
         @post = Post.new        
     end
+
+    def potential_to_follow
+        @potential_to_follow = User.potential_to_follow(current_user)
+    end
+
+    def following
+        @following = current_user.following
+    end
+
+    def followers
+        @followers = current_user.followers
+    end
 end
