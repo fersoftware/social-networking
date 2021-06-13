0106d7bef4bf5b56ec55568c080808b3b9bc220d
fersoftware <fersoftware@gmail.com>
2021-06-13 16:19:27 -0300

    timeline and user details

--- app/controllers/user/timeline_controller.rb
+++ app/controllers/user/timeline_controller.rb
@@ -1,5 +1,5 @@
 class User::TimelineController < UserController
     def index
-        
+        @posts = Post.where(user_id: current_user.following.pluck(:id)).order(created_at: :desc)
     end
 end
---
+++ app/controllers/user/users_controller.rb
@@ -0,0 +1,5 @@
+class User::UsersController < UserController
+    def show
+        @user = User.find(params[:id])
+    end
+end
