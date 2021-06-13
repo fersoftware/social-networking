d3412060c769a5c88a2617f9c8276ad4074f3093
fersoftware <fersoftware@gmail.com>
2021-06-13 13:16:53 -0300

    followers and following

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do    
     factory :user do
         name { "Fernando"}
-        email { "user@user.com"}
+        email { "user#{rand(1..100)}@user.com"}
         phone { "12345566"}
         avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/test-avatar.png'), 'image/png')}
         password { "123456"}
